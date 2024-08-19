fc55318993a38f8e21578b53a94cdf194e72cf71
Vinicius Silva Pereira <73970288+Vinicius2407@users.noreply.github.com>
2024-08-19 02:26:31 -0300

    feat: add auth provider

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,10 +1,20 @@
+"use client"
+
 import Image from "next/image"
+import { signIn, useSession } from "next-auth/react"
+
+import { MenuIcon } from "lucide-react"
 
 import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
-import { MenuIcon } from "lucide-react"
 
 export default function Header() {
+    const { data } = useSession();
+
+    const handleLogin = async () => {
+        await signIn()
+    }
+
     return (
         <Card>
             <CardContent className="flex flex-row items-center justify-between p-5">
@@ -17,6 +27,7 @@ export default function Header() {
                 <Button variant="outline" size="icon">
                     <MenuIcon size={18} />
                 </Button>
+
             </CardContent>
         </Card>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+
 import "./globals.css"
+
 import Footer from "./_components/footer"
+import AuthProvider from "./_providers/auth"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,8 +21,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${inter.className} dark`}>
-                {children}
-                <Footer />
+                <AuthProvider>
+                    {children}
+                    <Footer />
+                </AuthProvider>
             </body>
         </html>
     )
