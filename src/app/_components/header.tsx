fe4689172cffb86e429483e43d44b19647c96904
Vinicius Silva Pereira <73970288+Vinicius2407@users.noreply.github.com>
2024-08-19 02:27:37 -0300

    .

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Image from "next/image"
-import { signIn, useSession } from "next-auth/react"
 
 import { MenuIcon } from "lucide-react"
 
@@ -9,12 +8,6 @@ import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
 
 export default function Header() {
-    const { data } = useSession();
-
-    const handleLogin = async () => {
-        await signIn()
-    }
-
     return (
         <Card>
             <CardContent className="flex flex-row items-center justify-between p-5">
@@ -27,7 +20,6 @@ export default function Header() {
                 <Button variant="outline" size="icon">
                     <MenuIcon size={18} />
                 </Button>
-
             </CardContent>
         </Card>
     )
